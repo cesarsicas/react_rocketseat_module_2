545d182964666ee863ed036724275744330f3399
Julio Siqueira <julio.siqueira@redeceler.com.br>
2022-05-01 17:15:05 -0300

    using global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css';
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/globals';
+
+//criação de um componente chamado Title
+//substitui o h1 logo abaixo
+//usado para escrever css direto no js
+//const Title  = styled.h1`
+//            color: #FF0000`;
 
 export function App() {
   return (
     <div className="App">
         <h1 className='title'>Hellow brah</h1>
+
+        <GlobalStyle/>
     </div>
   );
 }
